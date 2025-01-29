03062e36f3387512a461edc23b5aac813b4e21c3
EmiRoberti77 <emi@datamotion.ai>
2025-01-29 16:53:00 +0000

    .env

--- bin/cdk_deploy_fargate.ts
+++ bin/cdk_deploy_fargate.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
+import * as dotenv from "dotenv";
 import * as cdk from "aws-cdk-lib";
 import { FargateOAIXStack } from "../lib/cdk_deploy_fargate_t3";
+
+dotenv.config();
 
 const app = new cdk.App();
 new FargateOAIXStack(app, "oaix-server-image-process-stack", {
   env: {
     region: "us-east-1",
-    account: "432599188850",
+    account: process.env.ACCOUNT!,
   },
 });
